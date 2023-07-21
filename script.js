55581592db80ae467c5fde4eec25792addc83978
Jurgita <jurgita.eicinaite@yahoo.com>
2023-07-21 20:33:15 +0300

    delete functionality

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
 'use strict';
 
-import listItem from './components/listItem.js';
-
-const initialData = [{ deadline: '', description: 'some not important task', finished: false }];
-let listItems;
-const text = document.getElementById('description').value;
-const endSelection = document.getElementById('end').value;
-console.log(endSelection);
+const initialData = [
+    { deadline: '', description: 'some not important task', finished: false, id: '0', finishDate: '', startDate: '' },
+];
+let taskList;
 
 const sessionKey = 'toDoList';
 if (!JSON.parse(sessionStorage.getItem(sessionKey))) {
-    listItems = initialData;
-    sessionStorage.setItem(sessionKey, JSON.stringify(listItems));
+    taskList = initialData;
+    sessionStorage.setItem(sessionKey, JSON.stringify(taskList));
 } else {
-    listItems = JSON.parse(sessionStorage.getItem(sessionKey));
+    taskList = JSON.parse(sessionStorage.getItem(sessionKey));
 }
 
-console.log(listItem);
-for (let i of listItems) {
-    listItem(i);
-}
+// generate list
+taskListGenerator();
+
 // Add event listener
-
 const btnAdd = document.getElementById('add');
 btnAdd.addEventListener('click', function (e) {
     e.preventDefault();
-
-    listItems.push({
+    const newItem = {
         deadline: document.getElementById('end').value,
         description: document.getElementById('description').value,
         finished: false,
-    });
-    sessionStorage.setItem(sessionKey, JSON.stringify(listItems));
+        id: `${taskList.length > 0 ? [...taskList].map(item => +item.id).sort((a, b) => b - a)[0] + 1 : '0'}`,
+    };
+    taskList.push(newItem);
+    sessionStorage.setItem(sessionKey, JSON.stringify(taskList));
+    document.getElementById('list').innerHTML = '';
+    taskListGenerator();
 });
 
-// const toDo = document.createElement('div');
-// const checkbox = document.createElement('input');
-// checkbox.setAttribute()
+function taskListGenerator() {
+    document.getElementById('list').innerHTML = null;
+    for (let item of taskList) {
+        const toDo = document.createElement('div');
+        toDo.setAttribute('class', 'listItem');
+        toDo.setAttribute('id', item.id);
+
+        const description = document.createElement('h3');
+        description.textContent = item.description;
+
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.checked = item.finished;
+
+        const endDate = document.createElement('p');
+        endDate.setAttribute('class', 'date');
+        endDate.textContent = item.deadline ? item.deadline.toLocaleString('lt-LT').split('T').join(' ') : 'N/A';
+
+        const deleteBtn = document.createElement('a');
+        deleteBtn.setAttribute('class', 'delete');
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', e => {
+            taskList.splice(
+                taskList.findIndex(item => '' + item.id === '' + e.target.parentElement.id),
+                1
+            );
+            sessionStorage.setItem('toDoList', JSON.stringify(taskList));
+            taskListGenerator();
+        });
+
+        toDo.appendChild(checkbox);
+        toDo.appendChild(description);
+        toDo.appendChild(endDate);
+        toDo.appendChild(deleteBtn);
+        document.querySelector('#list').appendChild(toDo);
+    }
+}
