8ea15b36605cdd84e92efc21e0ff9e2dc3b428e4
Jurgita <jurgita.eicinaite@yahoo.com>
2023-07-21 14:21:43 +0300

    sessionKey check, sorting in html

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+const initialData = [{ id: 1, dedaline: '', description: 'some not important task' }];
+let listItems;
+const endSelection = document.querySelector('#end').value;
+
+const sessionKey = 'toDoList';
+if (!JSON.parse(sessionStorage.getItem(sessionKey))) {
+    listItems = initialData;
+    sessionStorage.setItem(sessionKey, JSON.stringify(listItems));
+    console.log('newly set', listItems);
+} else {
+    listItems = JSON.parse(sessionStorage.getItem(sessionKey));
+    console.log('already set', listItems);
+}
