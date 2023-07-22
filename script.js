51bf093204edc723b5b379491f4788f427437f2f
Jurgita <jurgita.eicinaite@yahoo.com>
2023-07-22 11:27:14 +0300

    input alerts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import getTime from './components/getTime.js';
 import { initialData, sessionKey, sortKey } from './components/initialData.js';
 import taskListGenerator from './components/taskListGenerator.js';
 
@@ -22,16 +23,29 @@ const btnAdd = document.getElementById('add');
 
 btnAdd.addEventListener('click', function (e) {
     e.preventDefault();
-    const newItem = {
-        deadline: document.getElementById('end').value,
-        description: document.getElementById('description').value,
-        finished: false,
-        id: `${taskList.length > 0 ? [...taskList].map(item => +item.id).sort((a, b) => b - a)[0] + 1 : '0'}`,
-        startDate: Date.now(),
-    };
-    taskList.push(newItem);
-    sessionStorage.setItem(sessionKey, JSON.stringify(taskList));
-    taskListGenerator();
+    const textArea = document.getElementById('description').value;
+    const dateInput = document.getElementById('deadline').value;
+    if (
+        ![...textArea].some(
+            symbol => symbol.toLowerCase().charCodeAt() >= 97 && symbol.toLowerCase().charCodeAt() <= 122
+        )
+    ) {
+        alert('Input field should contain at least some letters!');
+    } else if (new Date(dateInput).getTime() <= Date.now()) {
+        const currentTime = new Date();
+        alert(`Task deadline cannot be earlier than present (${getTime(currentTime)}) time`);
+    } else {
+        const newItem = {
+            deadline: dateInput,
+            description: textArea,
+            finished: false,
+            id: `${taskList.length > 0 ? [...taskList].map(item => +item.id).sort((a, b) => b - a)[0] + 1 : '0'}`,
+            startDate: Date.now(),
+        };
+        taskList.push(newItem);
+        sessionStorage.setItem(sessionKey, JSON.stringify(taskList));
+        taskListGenerator();
+    }
 });
 
 // sorting
